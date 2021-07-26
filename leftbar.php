c6b564425570b46d8d4ec31aa60f3fab4d794f4e
Sushh0220 <87278838+Sushh0220@users.noreply.github.com>
2021-07-26 15:51:58 +0530

    Initial commit

---
+++ leftbar.php
@@ -0,0 +1,33 @@
+ <!-- BEGIN SIDEBAR -->
+  <div class="page-sidebar" id="main-menu">
+    <!-- BEGIN MINI-PROFILE -->
+    <div class="page-sidebar-wrapper scrollbar-dynamic" id="main-menu-wrapper">
+      <div> 
+        <div class="profile-wrapper" hidden="true" > <img src="assets/img/user.png"  alt="" data-src="assets/img/user.png" data-src-retina="assets/img/user.png" width="69" height="69" /> </div>
+        <div class="user-info">
+          <div class="greeting" style="font-size:14px;" hidden="true">Swapnil Shah & Associates</div>
+          <div class="username" style="font-size:12px;" hidden="true" ><?php echo $_SESSION['name'];?></div>
+          <div class="status" style="font-size:10px;" hidden="true"><a href="#">
+            <div class="status-icon green" hidden="true"></div>
+            Online</a></div> -->
+        </div>
+      </div>
+      <!-- END MINI-PROFILE -->
+      <!-- BEGIN SIDEBAR MENU -->
+      <!-- <p class="menu-title">BROWSE <span class="pull-right"><a href="javascript:;"><i class="fa fa-refresh"></i></a></span></p> -->
+   
+    <ul>	
+      <!-- <li class="start"> <a href="dashboard.php"> <i class="icon-custom-home"></i> <span class="title">Dashboard</span> <span class="selected"></span>  </a> 
+		    </li> -->
+    
+          <!-- <li><a href="change-password.php"><span class="fa fa-file-text-o"></span> Change Password</a></li>
+                            <li><a href="profile.php"><span class="fa fa-user"></span> Profile</a></li> -->
+                         
+                            
+                              <li class="start"><a href="get-quote.php"> <span class="fa fa-tasks"></span> Request a Quote</a></li>
+							     <!-- <li ><a href="create-ticket.php"><span class="fa fa-ticket"></span> Create Ticket</a></li>
+                                <li ><a href="view-tickets.php"><span class="fa fa-ticket"></span> View Ticket</a></li> -->
+                           
+    </ul>
+    
+	
